a1df54112c7f0a2d679fec144879e5ba7037b473
dua-qasim <duaqasim307@gmail.com>
2026-02-22 16:52:07 +0500

    second week task

---
+++ Week 1/task13.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+ system("color 03");
+cout<<"+--^-------------,----------,-------,-----------^-,   "<<endl;
+cout<<" | | | | | |      '----------'        |            0  "<<endl;
+cout<<" '+-----------------------------------^------------|  "<<endl;
+cout<<"   ' ,-----------,---------,---------------------'    "<<endl;
+cout<<"      / XXXXXXX /' |       /'                         "<<endl;  
+cout<<"     / XXXXXXX /   |      /'                          "<<endl;
+cout<<"    / XXXXXXX /'--------'                             "<<endl;
+cout<<"   / XXXXXXX /                                        "<<endl;
+cout<<"  / XXXXXXX /                                         "<<endl;
+cout<<" (_________(                                          "<<endl;
+cout<<"  '-------'                                           "<<endl;
+}
+              
---
+++ Week 1/task9.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<"--------------------------      "<<endl;
+cout<<"    o   ^__^                    "<<endl;
+cout<<"     o  (oo)\\__________        "<<endl;
+cout<<"                                "<<endl;
+cout<<"        (__)\\         )\\/\\   "<<endl;
+cout<<"            ||------W  |        "<<endl;
+cout<<"            ||        ||        "<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
